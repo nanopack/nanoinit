6fb6895af7cfaf440e09c47ddc783bc0dd2b11b5
Braxton Huggins <braxton@pagodabox.com>
2015-07-24 14:21:40 -0600

    some updates

--- nanoinit.c
+++ nanoinit.c
@@ -9,80 +9,83 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <dirent.h>
-#include "nanit.h"
+#include <stdarg.h>
+#include "nanoinit.h"
 
 extern char **environ;
 
 int log_level = 0;
-int signaled = 0;
-int alarmed = 0;
+int signaled  = 0;
+int alarmed   = 0;
 
-void
-error(char *message)
-{
-	if (log_level >= LOG_LEVEL_ERROR)
-		fprintf(stderr, "Error: %s\n", message);
+#define LOG(NAME, LOG_LEVEL, FORMAT_TITLE) \
+void \
+NAME(const char *format, ...) \
+{ \
+    va_list args; \
+    va_start(args, format); \
+	if (log_level >= LOG_LEVEL) { \
+		char *real_format = malloc(strlen(format) + strlen(FORMAT_TITLE) + 4); \
+		memset(real_format, 0, strlen(format) + strlen(FORMAT_TITLE) + 4); \
+		sprintf(real_format, FORMAT_TITLE": %s\n", format); \
+		vfprintf(stderr, real_format, args); \
+		free(real_format); \
+	} \
+	va_end(args); \
 }
 
-void
-warn(char *message)
-{
-	if (log_level >= LOG_LEVEL_WARN)
-		fprintf(stderr, "Warning: %s\n", message);
-}
-
-void
-info(char *message)
-{
-	if (log_level >= LOG_LEVEL_INFO)
-		fprintf(stderr, "Info: %s\n", message);
-}
-
-void
-debug(char *message)
-{
-	if (log_level >= LOG_LEVEL_DEBUG)
-		fprintf(stderr, "Debug: %s\n", message);
-}
+// Create functions for logging
+LOG(error, LOG_LEVEL_ERROR, "Error");
+LOG(warn,  LOG_LEVEL_WARN,  "Warning");
+LOG(info,  LOG_LEVEL_INFO,  "Info");
+LOG(debug, LOG_LEVEL_DEBUG, "Debug");
 
 void
 import_envvars(int clear_existing_environment, 
 	int override_existing_environment)
 {
+	debug("import_envvars(%d, %d)", clear_existing_environment, override_existing_environment);
 	struct stat s;
-	env_list *list = NULL;
+	env_list    *list = NULL;
+	char        env_dir[] = "/etc/container_environment/";
 	// populate the env_list
-	if (stat("/etc/container_environment", &s) == 0 && S_ISDIR( s.st_mode )) {
-		DIR *envdir = opendir("/etc/container_environment/");
-		env_list *current = list;
+	if (stat(env_dir, &s) == 0 && S_ISDIR( s.st_mode )) {
+		DIR           *envdir  = opendir(env_dir);
+		env_list      *current = list;
 		struct dirent *entry;
 		while (entry = readdir(envdir)) {
 			char filename[FILENAME_MAX];
-			sprintf(filename, "/etc/container_environment/", entry->d_name);
+			sprintf(filename, "%s%s", env_dir, entry->d_name);
 			if (stat(filename, &s) == 0 && S_ISREG(s.st_mode)) {
-				char *key = strdup(entry->d_name);
-				char *value = NULL;
-				char temp_value[4096];
-				FILE *file = fopen(filename, "r");
-				fscanf(file, "%s[^\n]", &temp_value);
-				fclose(file);
-				value = strdup(temp_value);
+				char *key   = strdup(entry->d_name);
+				char *value = malloc(s.st_size + 1);
+				memset(value, 0, s.st_size + 1);
+				FILE *file;
+				if ((file = fopen(filename, "r")) != NULL) {
+					fscanf(file, "%s[^\n]", value);
+					fclose(file);
+				} else {
+					error("Could not open %s: %s", filename, strerror(errno));
+				}
 				env_list *new_item = malloc(sizeof(env_list));
 				if (list == NULL)
 					list = new_item;
 				if (current)
 					current->next = new_item;
 				current = new_item;
-				current->key = key;
+				current->key   = key;
 				current->value = value;
-				current->next = NULL;
+				current->next  = NULL;
 			}
 		}
 		closedir(envdir);
+	} else {
+		error("Could not open %s: %s", env_dir, strerror(errno));
 	}
 
 	// clear the environment?
 	if (clear_existing_environment) {
+		// not portable
 		clearenv();
 	}
 	// set the environment?
@@ -147,40 +150,50 @@ char
 void
 export_envvars(int to_dir)
 {
-	FILE *shell_file = fopen("/etc/container_environment.sh", "w");
-	for (int i = 0; environ[i]; i++) {
-		char *envvar = environ[i];
-		// don't export it if it is one of the following:
-		// HOME, USER, GROUP, UID, GID, SHELL
+	debug("export_envvars(%d)", to_dir);
+	char shell_file_name[] = "/etc/container_environment.sh";
+	FILE *shell_file;
+	if ((shell_file = fopen(shell_file_name, "w")) != NULL) {
+		for (int i = 0; environ[i]; i++) {
+			char *envvar = environ[i];
+			// don't export it if it is one of the following:
+			// HOME, USER, GROUP, UID, GID, SHELL
 
-		if (strncmp("HOME=", envvar, 5) != 0 &&
-			strncmp("USER=", envvar, 5) != 0 &&
-			strncmp("GROUP=", envvar, 6) != 0 &&
-			strncmp("UID=", envvar, 4) != 0 &&
-			strncmp("GID=", envvar, 4) != 0 &&
-			strncmp("SHELL=", envvar, 6) != 0) {
-			size_t len = strstr(envvar, "=") - envvar;
-			char *key = malloc(len + 1);
-			memset(key, 0, len + 1);
-			strncpy(key, envvar, len);
-			char *value = strdup(envvar + len + 1);
-			char *safe_key = shquote(key);
-			char *safe_value = shquote(value);
-			if (to_dir) {
-				char filename[FILENAME_MAX];
-				sprintf(filename, "/etc/container_environment/%s", key);
-				FILE *file = fopen(filename, "w");
-				fprintf(file, "%s", value);
-				fclose(file);
+			if (strncmp("HOME=", envvar, 5) != 0 &&
+				strncmp("USER=", envvar, 5) != 0 &&
+				strncmp("GROUP=", envvar, 6) != 0 &&
+				strncmp("UID=", envvar, 4) != 0 &&
+				strncmp("GID=", envvar, 4) != 0 &&
+				strncmp("SHELL=", envvar, 6) != 0) {
+				size_t len = strstr(envvar, "=") - envvar;
+				char *key = malloc(len + 1);
+				memset(key, 0, len + 1);
+				strncpy(key, envvar, len);
+				char *value = strdup(envvar + len + 1);
+				char *safe_key = shquote(key);
+				char *safe_value = shquote(value);
+				if (to_dir) {
+					char filename[FILENAME_MAX];
+					sprintf(filename, "/etc/container_environment/%s", key);
+					FILE *file;
+					if ((file = fopen(filename, "w")) != NULL) {  
+						fprintf(file, "%s", value);
+						fclose(file);
+					} else {
+						error("Failed to open file %s: %s", filename, strerror(errno));
+					}
+				}
+				fprintf(shell_file, "export %s=%s\n", safe_key, safe_value);
+				free(key);
+				free(value);
+				free(safe_key);
+				free(safe_value);
 			}
-			fprintf(shell_file, "export %s=%s\n", safe_key, safe_value);
-			free(key);
-			free(value);
-			free(safe_key);
-			free(safe_value);
 		}
+		fclose(shell_file);
+	} else {
+		error("Failed to open file %s: %s", shell_file_name, strerror(errno));
 	}
-	fclose(shell_file);
 }
 
 void
@@ -205,8 +218,8 @@ kill_all_processes(int timeout)
 	int status = 0;
 
 	info("Killing all processes...");
-	(void) kill(-1, SIGTERM);
-	(void) alarm(timeout);
+	kill(-1, SIGTERM);
+	alarm(timeout);
 	while (done == 0 && alarmed == 0) {
 		// wait for all children
 		ret = waitpid(-1, &status, 0);
@@ -216,36 +229,50 @@ kill_all_processes(int timeout)
 	if (done == 0) {
 		warn("Not all processes have exited in time. Forcing them to exit.");
 		// try killing with stronger signal
-		(void) kill(-1, SIGKILL);
+		kill(-1, SIGKILL);
 	}
 	// cancel the alarm if it hasn't gone off yet
-	(void) alarm(0);
+	alarm(0);
 }
 
 pid_t
 run_init()
 {
+	debug("starting service manager")
 	pid_t init_pid;
-	char *args[] = {"/opt/gonano/sbin/runsvdir", "-P", "/etc/service", 0};
-	// char *args[] = {"/bin/sleep", "3600", 0};
+	// char *args[] = {"/opt/gonano/sbin/runsvdir", "-P", "/etc/service", 0};
+	char *args[] = {"/bin/sleep", "10", 0};
 
 	if ((init_pid = fork()) == 0) {
 		import_envvars(1, 1);
-		execve(args[0], args, environ);
+		if (execve(args[0], args, environ) == -1) {
+			error("Failed to exec the init process: %s", strerror(errno));
+			exit(1);
+		}
+	} else if (init_pid < 0) {
+		error("Failed to fork init process: %s", strerror(errno));
 	}
 
+	debug("init pid: %d", init_pid);
 	return init_pid;
 }
 
 int
 main(int argc, char *argv[])
 {
+	log_level = LOG_LEVEL_DEBUG;
+	info("                         _       _ _ ");  
+	info(" _ __   __ _ _ __   ___ (_)_ __ (_) |_ ");
+	info("| '_ \\ / _` | '_ \\ / _ \\| | '_ \\| | __|");
+	info("| | | | (_| | | | | (_) | | | | | | |_ ");
+	info("|_| |_|\\__,_|_| |_|\\___/|_|_| |_|_|\\__|");
+	info("booting...");
 	pid_t init_pid = 0;
 	pid_t child_pid = 0;
 	int status = 0;
 	int ret = 0;
 
-	log_level = LOG_LEVEL_INFO;
+
 	struct sigaction alarm;
 	struct sigaction other;
 	alarm.sa_handler = alarm_handler;
@@ -254,7 +281,7 @@ main(int argc, char *argv[])
 	other.sa_handler = ignore_signals;
 	sigfillset(&other.sa_mask);
 	other.sa_flags = 0;
-
+	debug("setting signal handlers");
 	sigaction(SIGALRM, &alarm, NULL);
 	sigaction(SIGINT, &other, NULL);
 	sigaction(SIGTERM, &other, NULL);
@@ -262,8 +289,7 @@ main(int argc, char *argv[])
 	import_envvars(0, 0);
 	export_envvars(1);
 
-	init_pid = run_init();
-	if (init_pid > 0) {
+	if ((init_pid = run_init()) > 0) {
 		while (child_pid != init_pid && signaled == 0) {
 			child_pid = waitpid(-1, &status, 0);
 		}
@@ -272,6 +298,6 @@ main(int argc, char *argv[])
 	if (signaled)
 		warn("Init system aborted.");
 
-	kill_all_processes(KILL_ALL_PROCESSES_TIMEOUT);
+	// kill_all_processes(KILL_ALL_PROCESSES_TIMEOUT);
 	return ret;
 }
--- nanoinit.h
+++ nanoinit.h
@@ -2,9 +2,9 @@
 #define KILL_ALL_PROCESSES_TIMEOUT 5
 
 #define LOG_LEVEL_ERROR 1
-#define LOG_LEVEL_WARN  1
-#define LOG_LEVEL_INFO  2
-#define LOG_LEVEL_DEBUG 3
+#define LOG_LEVEL_WARN  2
+#define LOG_LEVEL_INFO  3
+#define LOG_LEVEL_DEBUG 4
 
 typedef struct env_list_t {
 	char		*key;
